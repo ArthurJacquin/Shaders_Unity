22ab6be791c1d70382ce3ee63cf9f48d3a468877
ArthurJacquin <jacquin.arthur@outlook.fr>
2021-03-02 17:55:28 +0100

    Headlight signature time reset script + TMP

---
+++ Shaders_Unity/Assets/Scripts/ShaderTimeManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShaderTimeManager : MonoBehaviour
+{
+    [SerializeField] List<Renderer> renderers;
+    [SerializeField] int materialID;
+    private float time;
+
+    private void Update()
+    {
+        time += Time.deltaTime;
+
+        foreach (Renderer r in renderers)
+        {
+            r.materials[materialID].SetFloat("_TimeAnim", time);
+        }
+    }
+
+    public void ResetTime()
+    {
+        time = 0f;
+    }
+
+}
